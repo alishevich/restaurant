c0a69b5e85753efad3b268ac896752eac75441aa
Ekaterina Alishevich <ekaterina.alishevich@gmail.com>
2021-01-30 19:07:01 +0300

    add user controllers

--- src/main/java/org/example/web/user/ProfileController.java
+++ src/main/java/org/example/web/user/ProfileController.java
@@ -1,22 +1,36 @@
 package org.example.web.user;
 
 import org.example.model.User;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import static org.example.web.SecurityUtil.authUserId;
 
-@Controller
+@RestController
+@RequestMapping(value = ProfileController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProfileController extends AbstractUserController {
+    static final String REST_URL = "rest/profile";
 
+    @GetMapping
     public User get() {
         return super.get(authUserId());
     }
 
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete() {
         super.delete(authUserId());
     }
 
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(User user) {
         super.update(user, authUserId());
+    }
+
+    @GetMapping(value = "/text")
+    public String testUTF() {
+        return "Русский текст";
     }
 }
