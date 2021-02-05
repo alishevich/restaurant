40b238a41264242265698040440f9b23ebad78fe
Ekaterina Alishevich <ekaterina.alishevich@gmail.com>
2021-02-05 20:13:21 +0300

    add date formatter

---
+++ src/main/java/org/example/web/converter/DateFormatter.java
@@ -0,0 +1,28 @@
+package org.example.web.converter;
+
+import org.springframework.format.Formatter;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateFormatter {
+    public static class LocalDateFormatter implements Formatter<LocalDate> {
+
+        @Override
+        public LocalDate parse(String text, Locale locale) {
+            return parseLocalDate(text);
+        }
+
+        @Override
+        public String print(LocalDate lt, Locale locale) {
+            return lt.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+    }
+
+    public static @Nullable LocalDate parseLocalDate(@Nullable String str) {
+        return StringUtils.hasText(str) ? LocalDate.parse(str) : null;
+    }
+}
